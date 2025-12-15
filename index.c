e5dee43f3187a1c7e81b4cc8459ed2c48d1d28a8
Heer <heerpatel0501@gmail.com>
2025-12-15 11:20:08 +0530

    fixed first page

--- index.c
+++ index.c
@@ -1,5 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-    printf("Hello, World!\n");
+    int flightType;
+    printf("        AERONOVA AIRLINES\n");
+    printf("=================================\n");
+    printf(" Welcome to the AERONOVA Airlines\n");
+    printf("=================================\n");
+    printf("    AIRLINE RESERVATION SYSTEM\n");
+    printf("=================================\n");
+    printf("Select Flight Type:\n");
+    printf("1. Domestic Flight\n");
+    printf("2. International Flight\n");
+    printf("---------------------------------\n");
+    printf("Enter your choice: ");
+    scanf("%d", &flightType);
+
+    if (flightType == 1) {
+        printf("\nYou selected DOMESTIC FLIGHT.\n");
+    }
+    else if (flightType == 2) {
+        printf("\nYou selected INTERNATIONAL FLIGHT.\n");
+    }
+    else {
+        printf("\nInvalid choice! Please restart the program.\n");
+    }
+
     return 0;
 }
